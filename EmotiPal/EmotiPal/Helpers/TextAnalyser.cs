46fc507f79f2da0124496f4020332c7e4f6b4d57
Luce Carter <luce@lucyscarter.co.uk>
2021-04-17 12:05:55 +0100

    :sparkles: Implemented Text Sentiment Analysis

---
+++ EmotiPal/EmotiPal/Helpers/TextAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.TextAnalytics;
+using EmotiPal.Helpers;
+
+namespace EmotiPal.Helpers
+{
+    public class TextAnalyser
+    {
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential(AzureKeys.SentimentAPIKey);
+        private static readonly Uri endpoint = new Uri(AzureKeys.BaseUrl);
+        private readonly TextAnalyticsClient client = new TextAnalyticsClient(endpoint, credentials);
+
+        public TextAnalyser()
+        {
+        }
+
+        public async Task<string> AnalyseSentiment(string textToAnalyse)
+        {
+            DocumentSentiment documentSentiment = await client.AnalyzeSentimentAsync(textToAnalyse);
+
+             return documentSentiment.Sentiment.ToString();
+        }
+    }
+}
--- EmotiPal/EmotiPal/ViewModels/AnalyseTextPageViewModel.cs
+++ EmotiPal/EmotiPal/ViewModels/AnalyseTextPageViewModel.cs
@@ -1,11 +1,69 @@
-﻿using MvvmHelpers;
+﻿using EmotiPal.Helpers;
+using MvvmHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace EmotiPal.ViewModels
 {
     public class AnalyseTextPageViewModel : BaseViewModel
     {
+        private string _textForAnalysis;
+        private string _sentimentAnalysisResult;
+        private Color _sentimentResultColour;
+
+        public AnalyseTextPageViewModel()
+        {
+            AnalyseTextCommand = new Command(async () => await AnalyseText(TextForAnalysis));
+            SentimentResultColour = Color.Black;
+        }
+
+        public string TextForAnalysis
+        {
+            get => _textForAnalysis;
+            set => SetProperty(ref _textForAnalysis, value);
+        }
+
+        public ICommand AnalyseTextCommand { get; }
+
+        public string SentimentAnalysisResult
+        {
+            get => _sentimentAnalysisResult;
+            set => SetProperty(ref _sentimentAnalysisResult, value);
+        }
+
+        public Color SentimentResultColour
+        {
+            get => _sentimentResultColour;
+            set => SetProperty(ref _sentimentResultColour, value);
+        }
+
+        async Task AnalyseText(string textForAnalysis)
+        {
+            TextForAnalysis = "";
+            var textAnalyser = new TextAnalyser();
+            switch (await textAnalyser.AnalyseSentiment(textForAnalysis))
+            {
+                case "Unknown":
+                    SentimentResultColour = Color.Blue;
+                    SentimentAnalysisResult = "Hmmm I can't seem to tell how you are feeling!";
+                    break;
+                case "Normal":
+                    SentimentAnalysisResult = "You seem normal to me!";
+                    break;
+                case "Positive":
+                    SentimentResultColour = Color.Green;
+                    SentimentAnalysisResult = "Zip-a-dee-doo-dah, zip-a-dee-ay My, oh, my, what a wonderful day ";
+                    break;
+                case "Negative":
+                    SentimentResultColour = Color.Red;
+                    SentimentAnalysisResult = "Sucks to be you!";
+                    break;
+            }
+        }
+
     }
 }
