95089b63ff43e8bcdfa69148e9341a6efeb14c77
Luce Carter <luce@lucyscarter.co.uk>
2021-04-17 10:44:08 +0100

    :construction: Implemented MVVM Structure with MVVM Helpers Nuget Package

---
+++ EmotiPal/EmotiPal/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmHelpers;
+
+namespace EmotiPal.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+    }
+}
