4a01f9a6f5f2cec5c2c3fe263fdeb6b61eefc703
Luce Carter <luce@lucyscarter.co.uk>
2021-04-17 11:34:23 +0100

    :arrow_down: Added new page and view model

---
+++ EmotiPal/EmotiPal/Views/AnalyseBodyLanguagePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using EmotiPal.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmotiPal.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AnalyseBodyLanguagePage : ContentPage
+    {
+        public AnalyseBodyLanguagePage()
+        {
+            InitializeComponent();
+            BindingContext = new AnalyseBodyLanguagePageViewModel();
+        }
+    }
+}
