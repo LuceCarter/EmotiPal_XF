d060774b33c3a118f9af22f1c9ca83f6ec07b2d9
Luce Carter <luce@lucyscarter.co.uk>
2021-04-17 11:43:35 +0100

    :construction: Added Helper file for secret keys

---
+++ EmotiPal/EmotiPal/Helpers/AzureKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmotiPal.Helpers
+{
+    public static class AzureKeys
+    {
+        public const string FaceApiKey = "<Your API Key>";
+        public const string BaseUrl = "<Your Base URL from Azure>";
+        public const string SentimentAPIKey = "<Your Sentiment API Key>";
+    }
+}
