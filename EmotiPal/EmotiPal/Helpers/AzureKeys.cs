486b4e046ed39983e888f03a40a90b1a2a769e17
Luce Carter <luce@lucyscarter.co.uk>
2021-07-05 12:57:56 +0100

    Added missing semi-colon

--- EmotiPal/EmotiPal/Helpers/AzureKeys.cs
+++ EmotiPal/EmotiPal/Helpers/AzureKeys.cs
@@ -9,6 +9,6 @@ namespace EmotiPal.Helpers
         public const string FaceApiKey = "<Your API Key>";
         public const string BaseUrl = "<Your Base URL from Azure>";
         public const string SentimentAPIKey = "<Your Sentiment API Key>";
-        public const string BlobStorageConnectionString = "<Your Blob Storage Account Connection String>"
+        public const string BlobStorageConnectionString = "<Your Blob Storage Account Connection String>";
     }
 }
